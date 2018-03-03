0753fcea20bc74d232c6f505fe78f3b1d4ad10a5
Mishael <mishael.harry@cyberspace.net.ng>
2018-03-03 17:40:18 +0100

    Completed flash chat

---
+++ app/src/main/java/com/app/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,33 @@
+package com.app.flashchatnewfirebase;
+
+/**
+ * Created by Mishael on 3/3/2018.
+ */
+
+public class InstantMessage {
+    private  String message, author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
